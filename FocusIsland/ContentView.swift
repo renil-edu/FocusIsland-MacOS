165d077ad222c21349e6f327ef5baa889b6ea010
renil-edu <renil.edu@gmail.com>
2025-08-02 21:04:04 -0500

    Have notch displaying intro text, succesfully integrated with DynamicNotch API

--- FocusIsland/ContentView.swift
+++ FocusIsland/ContentView.swift
@@ -5,20 +5,23 @@
 //  Created by UT Austin on 8/2/25.
 //
 
+
+
+
+
+
+
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        Text("🎉 R is quite a genius! 🎉")
+            .font(.title)
+            .padding()
+            .background(
+                RoundedRectangle(cornerRadius: 24)
+                    .fill(Color(.sRGB, white: 0.15, opacity: 0.96))
+            )
+            .foregroundColor(.white)
     }
-}
-
-#Preview {
-    ContentView()
 }
--- FocusIsland/FocusIslandApp.swift
+++ FocusIsland/FocusIslandApp.swift
@@ -5,13 +5,31 @@
 //  Created by UT Austin on 8/2/25.
 //
 
+
+
+
 import SwiftUI
+import DynamicNotchKit
 
 @main
 struct FocusIslandApp: App {
+    @NSApplicationDelegateAdaptor(AppDelegate.self) var appDelegate
+
     var body: some Scene {
-        WindowGroup {
-            ContentView()
+        // This doesn't create a real window unless the user opens app settings.
+        Settings { EmptyView() }
+    }
+}
+
+class AppDelegate: NSObject, NSApplicationDelegate {
+    var notch: DynamicNotch<ContentView, EmptyView, EmptyView>?
+
+    func applicationDidFinishLaunching(_ notification: Notification) {
+        // This is called when your app launches, before any SwiftUI scene appears
+        Task { @MainActor in
+            guard notch == nil else { return }
+            notch = DynamicNotch(expanded: { ContentView() })
+            await notch?.expand()
         }
     }
 }
